e3ba453bd99529ad9eca13d70f444ef835fa985f
alancho397 <alancho397@gmail.com>
2022-06-07 20:15:31 -0500

    Added decimal functionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const screen = document.querySelector('.screen');
 const clearButton = document.querySelector('.clear');
 const equalButton = document.querySelector('.equal');
 const inverseButton = document.querySelector('.inverse');
+const decimalButton = document.querySelector('.decimal');
 const operatorButtons = document.querySelectorAll('.operator');
 const numberButtons = document.querySelectorAll('.number');
 
@@ -33,37 +34,50 @@ function updateOperators(event){
         equal();
     }
     firstValue = screen.innerHTML;
-    clear();
+    screen.innerHTML = '';
     screen.innerHTML = event.target.innerHTML;
     operatorValue = event.target.innerHTML;
 }
 
 function clear(){
     screen.innerHTML = 0;
+    firstValue = 0;
+    secondValue = 0;
+    operatorValue = '';
 }
 
 function equal(){
     secondValue = screen.innerHTML;
     if(operatorValue === '+'){
-        screen.innerHTML = parseInt(firstValue) + parseInt(secondValue);
+        screen.innerHTML = parseFloat(firstValue) + parseFloat(secondValue);
     } else if(operatorValue === '-'){
-        screen.innerHTML = parseInt(firstValue) - parseInt(secondValue);
+        screen.innerHTML = parseFloat(firstValue) - parseFloat(secondValue);
     } else if(operatorValue === '*'){
-        screen.innerHTML = parseInt(firstValue) * parseInt(secondValue);
+        screen.innerHTML = parseFloat(firstValue) * parseFloat(secondValue);
     } else if(operatorValue === '/'){
-        screen.innerHTML = parseInt(firstValue) / parseInt(secondValue);
+        screen.innerHTML = parseFloat(firstValue) / parseFloat(secondValue);
     }
 }
 
 function inverse(){
-    if(Math.sign(parseInt(screen.innerHTML)) === 1){
-        screen.innerHTML = '-' + screen.innerHTML
+    if(Math.sign(parseFloat(screen.innerHTML)) === 1){
+        screen.innerHTML = '-' + screen.innerHTML;
+    } else {
+        screen.innerHTML = screen.innerHTML.substring(1);
     }
 }
 
+function decimal(){
+    if(screen.innerHTML.includes('.')){
+        return;
+    } else{
+        screen.innerHTML = screen.innerHTML + '.';
+    }
+}
 
 clearButton.addEventListener('click', clear);
 equalButton.addEventListener('click', equal);
 inverseButton.addEventListener('click', inverse);
+decimalButton.addEventListener('click', decimal);
 operatorButtons.forEach(button => button.addEventListener('click', updateOperators));
 numberButtons.forEach(button => button.addEventListener('click', updateNumbers));
