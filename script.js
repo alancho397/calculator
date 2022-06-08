dfeebd38da7bfb0b969d559467c3009203e62610
alancho397 <alancho397@gmail.com>
2022-06-07 19:42:13 -0500

    Added operators' functionality

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const screen = document.querySelector('.screen');
 const clearButton = document.querySelector('.clear');
+const equalButton = document.querySelector('.equal');
 const operatorButtons = document.querySelectorAll('.operator');
 const numberButtons = document.querySelectorAll('.number');
 
 let firstValue = 0;
 let secondValue = 0;
+let operatorValue = '';
 
 function updateNumbers(event){
-    if(screen.innerHTML[0] === 0){
-        console.log('hi');
-        //screen.innerHTML = event.target.innerHTML;
+    if(screen.innerHTML[0] === '0'){
+        screen.innerHTML = event.target.innerHTML;
+    } else if  (screen.innerHTML[0] === '+' || 
+                screen.innerHTML[0] === '-' || 
+                screen.innerHTML[0] === '*' || 
+                screen.innerHTML[0] === '/'){
+        screen.innerHTML = '';  
+        screen.innerHTML = screen.innerHTML + event.target.innerHTML;
+    } else{
+        screen.innerHTML = screen.innerHTML + event.target.innerHTML;
     }
-    screen.innerHTML = screen.innerHTML + event.target.innerHTML;
     //how do you know if input is first or second value?
 }
+
 
 function updateOperators(event){
     firstValue = screen.innerHTML;
     clear();
     screen.innerHTML = event.target.innerHTML;
+    operatorValue = event.target.innerHTML;
 }
 
 function clear(){
@@ -26,8 +36,20 @@ function clear(){
 }
 
 function equal(){
+    secondValue = screen.innerHTML;
+    if(operatorValue === '+'){
+        screen.innerHTML = parseInt(firstValue) + parseInt(secondValue);
+    } else if(operatorValue === '-'){
+        screen.innerHTML = parseInt(firstValue) - parseInt(secondValue);
+    } else if(operatorValue === '*'){
+        screen.innerHTML = parseInt(firstValue) * parseInt(secondValue);
+    } else if(operatorValue === '/'){
+        screen.innerHTML = parseInt(firstValue) / parseInt(secondValue);
+    }
 }
 
+
 clearButton.addEventListener('click', clear);
+equalButton.addEventListener('click', equal);
 operatorButtons.forEach(button => button.addEventListener('click', updateOperators));
 numberButtons.forEach(button => button.addEventListener('click', updateNumbers));
